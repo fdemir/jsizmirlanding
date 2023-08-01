f9c4242366a399adf95e233fdda7bc773473c593
fdemir <frndemir@gmail.com>
2023-08-01 06:22:05 +0300

    fix: remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jsizmir.github.io",
-  base: "/landing",
+  // base: "/landing",
   integrations: [tailwind()],
 });
